aedbe9023b4012c02e0000881eb11fcdc720c5d6
AdityaRajyaguru <adirajyaguru@gmail.com>
2020-10-15 13:26:36 -0400

    Added a skeleton main.cpp file outlining what we need to do.

---
+++ Sequential/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+//Function prototypes
+int euclideanDist(int x1[], int x2[], int sizeX1);
+
+int main() {
+    /* TODO
+     - Implement a function to calculate the euclidean distance
+     - Sort the list generated from all distances
+     - Initialize K to some integer and take the first k distances from the sorted list.
+     - Figure out what the K-points are corresponding to each distance
+     */
+    return 0;
+}
+
+int euclideanDist(int x1[], int x2[], int sizeX1) {
+    return 0;
+}
